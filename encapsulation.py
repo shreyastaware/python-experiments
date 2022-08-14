bed5e7c485a6df1c62e29d3a877743372b7f526e
Shreyas Taware <shreyastaware6@gmail.com>
2022-08-14 23:41:35 +0530

    added description to encapsulation.py file

--- encapsulation.py
+++ encapsulation.py
@@ -6,6 +6,21 @@ Created on Sun Aug 14 21:29:50 2022
 @author: shreyastaware
 """
 
+# =============================================================================
+# Encapsulation
+# Encapsulation is one of the fundamental concepts in object-oriented
+#  programming (OOP). It describes the idea of wrapping data and the methods
+#  that work on data within one unit. This puts restrictions on accessing
+#  variables and methods directly and can prevent the accidental modification
+#  of data. To prevent accidental change, an object’s variable can only be
+#  changed by an object’s method. Those types of variables are known as
+#  private variables.
+# 
+# A class is an example of encapsulation as it encapsulates all the data that
+#  is member functions, variables, etc.
+# 
+# =============================================================================
+
 # Python program to
 # demonstrate private members
 
